d7485b435451ce932d21bfb2d25d3bd6976d458c
JheisonQuiroga <jheisonquiroga7@gmail.com>
2026-02-20 19:31:36 -0500

    Add mouse tracking effect and background mask to enhance interactivity

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 const yearSpan = document.querySelector(".year");
 const nowDate = new Date(Date.UTC(2026, 1, 12));
+const body = document.querySelector("body");
 
 const options = {
   year: "numeric",
@@ -8,3 +9,14 @@ const options = {
 }
 
 yearSpan.textContent = nowDate.toLocaleString("es-ES", options);
+
+
+body.addEventListener("mousemove", (e) => {
+  // Obtenemos la posición del mouse (X e Y)
+  const x = e.clientX;
+  const y = e.clientY;
+  
+  // Enviamos estas coordenadas a las variables CSS del body
+  body.style.setProperty('--x', `${x}px`);
+  body.style.setProperty('--y', `${y}px`);
+});
